17cf0b012194114cd903058a73900902ffd7d0e2
KetvonRyn <ketlinvonryn@gmail.com>
2024-02-12 00:38:53 -0300

    versaodecorada

--- scriptsCalc.js
+++ scriptsCalc.js
@@ -57,27 +57,8 @@ function pegarValores() {
 }
 function CalculoPRICE(valEmp, parc, tx, Amort) {
     //seleciona e limpa o campo da resposta
-    result = document.getElementById("resultado");
+    result = document.getElementById("corpo-tabela");
     result.innerHTML = '';
-    // Cria o elemento tabela com id "result"
-    novaTabela = document.createElement('table');
-    novaTabela.id = "result";
-    result.appendChild(novaTabela);
-
-    // Cria abeçalho da tabela
-    var cabecalho = novaTabela.createTHead();
-    var linhaCabecalho = cabecalho.insertRow();
-    var colNumParcela = linhaCabecalho.insertCell(0);
-    var colValorParcela = linhaCabecalho.insertCell(1);
-    var colJuros = linhaCabecalho.insertCell(2);
-    var colAmortizacao = linhaCabecalho.insertCell(3);
-    var colSaldoDevedor = linhaCabecalho.insertCell(4);
-    // preenche cabecalho com dados
-    colNumParcela.textContent = "Número da Parcela";
-    colValorParcela.textContent = "Valor da Parcela";
-    colJuros.textContent = "Juros";
-    colAmortizacao.textContent = "Amortizações";
-    colSaldoDevedor.textContent = "Saldo Devedor";
 
     // Variáveis iniciais para soma no final
     var totalValorParcela = 0;
@@ -88,45 +69,49 @@ function CalculoPRICE(valEmp, parc, tx, Amort) {
     // Loop para calcular cada linha de parcelas 
     for (let i = 1; (i <= parc) && (saldoDevedor > 0); i++) {
         //cria uma nova linha na tabela
-        let novaLinha = novaTabela.insertRow();
+        let novaLinha = result.insertRow();
 
         //cria a celula 1 - NUMERO DE PARCELA
         let celNumParcela = novaLinha.insertCell(0);
         celNumParcela.textContent = i // preenche celula 1
 
-        //cria a celula 2 - VALOR DA PARCELA
-        let celValorParcela = novaLinha.insertCell(1);
+        //criar comando da celula 2
+        let celAmortExtra = novaLinha.insertCell(1);
+
+
+        //cria a celula 3 - VALOR DA PARCELA
+        let celValorParcela = novaLinha.insertCell(2);
         let fator = ((1 + tx) ** (parc - i + 1)); // fator é para evitar repetir duas vezes o valor na fórmula
         var valorParcela = (saldoDevedor * fator * tx) / (fator - 1) + (i < parc ? Amort : 0); // Adiciona Amortização apenas se não for a última parcela
         celValorParcela.textContent = valorParcela.toFixed(2); // preenche a célula
         totalValorParcela += valorParcela; // soma para mostrar no final
 
-
-        //cria celula 3 - JUROS
-        let celJuros = novaLinha.insertCell(2);
+        //cria celula 4 - JUROS
+        let celJuros = novaLinha.insertCell(3);
         var valJuros = saldoDevedor * tx //calculo
         celJuros.textContent = valJuros.toFixed(2) //preenche a celula
         totalJuros += valJuros // soma para mostrar no final
 
-        //cria celula 4 - AMORTIZACOES
-        let celAmortizacao = novaLinha.insertCell(3);
+        //cria celula 5 - AMORTIZACOES
+        let celAmortizacao = novaLinha.insertCell(4);
         var valorAmort = (valorParcela - valJuros) //calcula
         celAmortizacao.textContent = parseFloat(valorAmort).toFixed(2) // preenche a celula
         totalAmortizacao += valorAmort; // soma para mostrar no final
 
-        //cria celula 5 - SALDO DEVEDOR
-        let celSaldoDevedor = novaLinha.insertCell(4);
+        //cria celula 6 - SALDO DEVEDOR
+        let celSaldoDevedor = novaLinha.insertCell(5);
         saldoDevedor -= valorAmort// calcula
         celSaldoDevedor.textContent = parseFloat(saldoDevedor).toFixed(2) //preenche
     }
 
     // Adiciona a última linha "TOTAL" na tabela
-    let linhaTotal = novaTabela.insertRow();
+    let linhaTotal = result.insertRow(); linhaTotal.id = "linhaTotal";
     let celulaTotal = linhaTotal.insertCell(0); celulaTotal.textContent = "Total pago >>";
-    let celValorParcela = linhaTotal.insertCell(1);
-    let celJuros = linhaTotal.insertCell(2); celJuros.textContent = totalJuros.toFixed(2);
-    let celAmortizacao = linhaTotal.insertCell(3); celAmortizacao.textContent = parseFloat(totalAmortizacao).toFixed(2);
-    let celSaldoDevedor = linhaTotal.insertCell(4); celSaldoDevedor.textContent = "<< <<";
+    let celAmortExtra = linhaTotal.insertCell(1);
+    let celValorParcela = linhaTotal.insertCell(2);//preenchimento esta no if abaixo
+    let celJuros = linhaTotal.insertCell(3); celJuros.textContent = totalJuros.toFixed(2);
+    let celAmortizacao = linhaTotal.insertCell(4); celAmortizacao.textContent = parseFloat(totalAmortizacao).toFixed(2);
+    let celSaldoDevedor = linhaTotal.insertCell(5); celSaldoDevedor.textContent = "<< <<";
 
     //se o ultimo saldo devedor for negativo, deduz ele no valor pago nas parcelas
     if (saldoDevedor < 0) {
@@ -136,28 +121,9 @@ function CalculoPRICE(valEmp, parc, tx, Amort) {
     }
 }
 function CalculoSAC(valEmp, parc, tx, Amort) {
-    // Seleciona e limpa o campo da resposta
-    result = document.getElementById("resultado");
+    ///seleciona e limpa o campo da resposta
+    result = document.getElementById("corpo-tabela");
     result.innerHTML = '';
-    // Cria o elemento tabela com id "result"
-    novaTabela = document.createElement('table');
-    novaTabela.id = "result";
-    result.appendChild(novaTabela);
-
-    // Cria cabeçalho da tabela
-    var cabecalho = novaTabela.createTHead();
-    var linhaCabecalho = cabecalho.insertRow();
-    var colNumParcela = linhaCabecalho.insertCell(0);
-    var colValorParcela = linhaCabecalho.insertCell(1);
-    var colJuros = linhaCabecalho.insertCell(2);
-    var colAmortizacao = linhaCabecalho.insertCell(3);
-    var colSaldoDevedor = linhaCabecalho.insertCell(4);
-    // Preenche cabeçalho com dados
-    colNumParcela.textContent = "Número da Parcela";
-    colValorParcela.textContent = "Valor da Parcela";
-    colJuros.textContent = "Juros";
-    colAmortizacao.textContent = "Amortizações";
-    colSaldoDevedor.textContent = "Saldo Devedor";
 
     // Variáveis iniciais para soma no final
     var totalValorParcela = 0;
@@ -171,42 +137,46 @@ function CalculoSAC(valEmp, parc, tx, Amort) {
     // Loop para calcular cada linha de parcelas 
     for (let i = 1; (i <= parc) && (saldoDevedor > 0); i++) {
         // Cria uma nova linha na tabela
-        let novaLinha = novaTabela.insertRow();
+        let novaLinha = result.insertRow();
 
         // Cria a célula 1 - NÚMERO DE PARCELA
         let celNumParcela = novaLinha.insertCell(0);
         celNumParcela.textContent = i; // Preenche célula 1
 
-        // Cria a célula 2 - VALOR DA PARCELA
-        let celValorParcela = novaLinha.insertCell(1);
+        //criar comando da celula 2
+        let celAmortExtra = novaLinha.insertCell(1);
+
+        // Cria a célula 3 - VALOR DA PARCELA
+        let celValorParcela = novaLinha.insertCell(2);
         var valorParcela = amortizacao + saldoDevedor * tx; // Calcula o valor da parcela
         celValorParcela.textContent = valorParcela.toFixed(2); // Preenche a célula
         totalValorParcela += valorParcela; // Soma para mostrar no final
 
-        // Cria célula 3 - JUROS
-        let celJuros = novaLinha.insertCell(2);
+        // Cria célula 4 - JUROS
+        let celJuros = novaLinha.insertCell(3);
         var valJuros = saldoDevedor * tx; // Cálculo dos juros
         celJuros.textContent = valJuros.toFixed(2); // Preenche a célula
         totalJuros += valJuros; // Soma para mostrar no final
 
-        // Cria célula 4 - AMORTIZAÇÕES
-        let celAmortizacao = novaLinha.insertCell(3);
+        // Cria célula 5 - AMORTIZAÇÕES
+        let celAmortizacao = novaLinha.insertCell(4);
         celAmortizacao.textContent = amortizacao.toFixed(2); // Preenche a célula
         totalAmortizacao += amortizacao; // Soma para mostrar no final
 
-        // Cria célula 5 - SALDO DEVEDOR
-        let celSaldoDevedor = novaLinha.insertCell(4);
+        // Cria célula 6 - SALDO DEVEDOR
+        let celSaldoDevedor = novaLinha.insertCell(5);
         saldoDevedor -= amortizacao; // Cálculo do saldo devedor
         celSaldoDevedor.textContent = saldoDevedor.toFixed(2); // Preenche a célula
     }
 
     // Adiciona a última linha "TOTAL" na tabela
-    let linhaTotal = novaTabela.insertRow();
+    let linhaTotal = result.insertRow(); linhaTotal.id = "linhaTotal";
     let celulaTotal = linhaTotal.insertCell(0); celulaTotal.textContent = "Total pago >>";
-    let celValorParcela = linhaTotal.insertCell(1);
-    let celJuros = linhaTotal.insertCell(2); celJuros.textContent = totalJuros.toFixed(2);
-    let celAmortizacao = linhaTotal.insertCell(3); celAmortizacao.textContent = parseFloat(totalAmortizacao).toFixed(2);
-    let celSaldoDevedor = linhaTotal.insertCell(4); celSaldoDevedor.textContent = "<< <<";
+    let celAmortExtra = linhaTotal.insertCell(1)
+    let celValorParcela = linhaTotal.insertCell(2); //preenchimento esta no if abaixo
+    let celJuros = linhaTotal.insertCell(3); celJuros.textContent = totalJuros.toFixed(2);
+    let celAmortizacao = linhaTotal.insertCell(4); celAmortizacao.textContent = parseFloat(totalAmortizacao).toFixed(2);
+    let celSaldoDevedor = linhaTotal.insertCell(5); celSaldoDevedor.textContent = "<< <<";
 
     // Se o último saldo devedor for negativo, deduz ele no valor pago nas parcelas
     if (saldoDevedor < 0) {
